c9d1117d55986731a4303c500be8be9dd8204eec
admiswalker <admiswalker@gmail.com>
2019-07-24 23:47:44 +0900

    add test

--- test_vvec.hpp
+++ test_vvec.hpp
@@ -1,6 +1,6 @@
 #pragma once
 
-void TEST__vvec__Tr(){
+void TEST__vvec__Tr_00(){
 	std::vector<std::vector<int>> vvec = {
 		{ 1,  2,  3,  4}, 
 		{ 5,  6}, 
@@ -14,21 +14,26 @@ void TEST__vvec__Tr(){
 	};
 	std::vector<std::vector<int>> vvecTr = sstd::Tr<int>(vvec);
 	sstd::printn(vvecTr);
-
-//	vvecTr = [[ 1  5  7 15 18 20 24 26 29]
-//            [ 2  6  8 16 19 21 25 27]
-//            [ 3  0  9 17  0 22  0 28]
-//            [ 4  0 10  0  0 23]
-//            [ 0  0 11]
-//            [ 0  0 12]
-//            [ 0  0 13]
-//            [ 0  0 14]]
-
-
-// 後は間違えそうなテストをいっぱい書いてみて，通れば良しとしよう．
+	
+	std::vector<std::vector<int>> vvecAns = {
+		{ 1,  5,  7, 15, 18, 20, 24, 26, 29},
+		{ 2,  6,  8, 16, 19, 21, 25, 27}, 
+		{ 3,  0,  9, 17,  0, 22,  0, 28},
+		{ 4,  0, 10,  0,  0, 23},
+		{ 0,  0, 11},
+		{ 0,  0, 12},
+		{ 0,  0, 13},
+		{ 0,  0, 14}
+	};
+	
+	ASSERT_TRUE(vvecTr == vvecAns);
+	ASSERT_TRUE((int)123 == (int)123);
 }
 
 //------------------------------------------
 
-TEST(vvec, Tr){ TEST__vvec__Tr(); }
+// 後は間違えそうなテストをいっぱい書いてみて，通れば良しとしよう．
+
+
+TEST(vvec, Tr_00){ TEST__vvec__Tr_00(); }
 
