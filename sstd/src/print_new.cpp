d5947a842f309bbee2fec152ef7b78eeb0e07dcc
admiswalker <admiswalker@gmail.com>
2021-01-03 23:47:25 +0900

    wip

--- sstd/src/print_new.cpp
+++ sstd/src/print_new.cpp
@@ -1,209 +1,63 @@
 ﻿#include "./print_new.hpp"
 #include "./ssprintf.hpp"
+#include "./strmatch.hpp" // for dev
 //#include <stdio.h>
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
-/*
-namespace sstd_print_new{ // This namespace is for internal processing
-    std::string val2str(const        bool  rhs){ return sstd::ssprintf("%c", (rhs ? 'T' : 'F')); }
-    std::string val2str(const        char  rhs){ return sstd::ssprintf("'%c'", rhs); }
-//    std::string val2str(const       uchar  rhs){ return sstd::ssprintf("%u",   rhs); } // same as uint8
-    std::string val2str(const       int8   rhs){ return sstd::ssprintf("%d",    rhs); }
-    std::string val2str(const       int16  rhs){ return sstd::ssprintf("%d",    rhs); }
-    std::string val2str(const       int32  rhs){ return sstd::ssprintf("%d",    rhs); }
-    std::string val2str(const       int64  rhs){ return sstd::ssprintf("%lld",  rhs); }
-    std::string val2str(const      uint8   rhs){ return sstd::ssprintf("%u",    rhs); } // same as uchar
-    std::string val2str(const      uint16  rhs){ return sstd::ssprintf("%u",    rhs); }
-    std::string val2str(const      uint32  rhs){ return sstd::ssprintf("%u",    rhs); }
-    std::string val2str(const      uint64  rhs){ return sstd::ssprintf("%llu",  rhs); }
-//    std::string val2str(const       float  rhs){ return sstd::ssprintf("%.4f",  rhs); }
-    std::string val2str(const       float  rhs){
-        std::string ret = sstd::ssprintf("%g", rhs);
-        for(uint64 i=0; i<ret.size(); ++i){ if(ret[i]=='.'){return ret;} }
-        return ret+'.';
-    }
-//    std::string val2str(const      double  rhs){ return sstd::ssprintf("%.4lf", rhs); }
-    std::string val2str(const      double  rhs){
-        std::string ret = sstd::ssprintf("%g", rhs);
-        for(uint64 i=0; i<ret.size(); ++i){ if(ret[i]=='.'){return ret;} }
-        return ret+'.';
-    }
-    std::string val2str(const std::string& rhs){ return '\"'+rhs+'\"'; }
-    
-                          void print(const bool  rhs); // specealization
-                          void print(const char* rhs); // specealization
-    template <typename T> void print(const                         T  & rhs);
-    template <typename T> void print(const             std::vector<T> & rhs);
-    template <typename T> void print(const std::vector<std::vector<T>>& rhs);
-    
-                          void for_printn_new(const bool  rhs); // specealization
-                          void for_printn_new(const char* rhs); // specealization
-    template <typename T> void for_printn_new(const                         T  & rhs);
-    template <typename T> void for_printn_new(const             std::vector<T> & rhs);
-    template <typename T> void for_printn_new(const std::vector<std::vector<T>>& rhs);
+
+// woLF: without line feed code ('\n')
+void sstd::print_woLF_new(const  bool  rhs){ printf("%c", (rhs ? 'T' : 'F')); }
+void sstd::print_woLF_new(const  char  rhs){ printf("'%c'", rhs); }
+void sstd::print_woLF_new(const  int8  rhs){ printf("%d", rhs); }
+void sstd::print_woLF_new(const  int16 rhs){ printf("%d", rhs); }
+void sstd::print_woLF_new(const  int32 rhs){ printf("%d", rhs); }
+void sstd::print_woLF_new(const  int64 rhs){ printf("%ld", rhs); }
+void sstd::print_woLF_new(const uint8  rhs){ printf("%u", rhs); }
+void sstd::print_woLF_new(const uint16 rhs){ printf("%u", rhs); }
+void sstd::print_woLF_new(const uint32 rhs){ printf("%u", rhs); }
+void sstd::print_woLF_new(const uint64 rhs){ printf("%lu", rhs); }
+void sstd::print_woLF_new(const float  rhs){
+    std::string s = sstd::ssprintf("%g", rhs);
+    if(! sstd::charIn('.', s) ){ s += '.'; }
+    printf("%s", s.c_str());
 }
-*/
+void sstd::print_woLF_new(const double  rhs){
+    std::string s = sstd::ssprintf("%g", rhs); // Note: Prioritize readability and do not display all effective digits of double-precision type.
+    if(! sstd::charIn('.', s) ){ s += '.'; }
+    printf("%s", s.c_str());
+}
+void sstd::print_woLF_new(const        char* rhs){ printf("\"%s\"", rhs); }
+void sstd::print_woLF_new(const std::string& rhs){ printf("\"%s\"", rhs.c_str()); }
+    
+// with line feed code ('\n')
+void sstd::print_new(const  bool  rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const  char  rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const  int8  rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const  int16 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const  int32 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const  int64 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const uint8  rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const uint16 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const uint32 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const uint64 rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const float  rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const double rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const        char* rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+void sstd::print_new(const std::string& rhs){ sstd::print_woLF_new(rhs); printf("\n"); }
+    
+void sstd::for_printn_new( bool  rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( char  rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( int8  rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( int16 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( int32 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( int64 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(uint8  rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(uint16 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(uint32 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(uint64 rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new( float rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(double rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(const        char* rhs){ printf(" = "); sstd::print_new(rhs); }
+void sstd::for_printn_new(const std::string& rhs){ printf(" = "); sstd::print_new(rhs); }
+
 //-----------------------------------------------------------------------------------------------------------------------------------------------
-/*
-                      void sstd_print_new::print(const bool  rhs){ printf("%s\n", (rhs ? "true" : "false"));   }
-                      void sstd_print_new::print(const char* rhs){ printf("\"%s\"\n", rhs);                    }
-template <typename T> void sstd_print_new::print(const             T & rhs){ printf("%s\n", sstd_print_new::val2str(rhs).c_str()); }
-template <typename T> void sstd_print_new::print(const std::vector<T>& rhs){
-    printf("[");
-    if(rhs.size()>=1){
-        for(uint i=0; i<rhs.size()-1; ++i){ printf("%s ", sstd_print_new::val2str(rhs[i]).c_str()); }
-        printf("%s", sstd_print_new::val2str(rhs[rhs.size()-1]).c_str());
-    }
-    printf("]\n");
-}
-//---
-                      void sstd_print_new::for_printn_new(const bool  rhs){ printf(" = %s\n", (rhs ? "true" : "false"));   }
-                      void sstd_print_new::for_printn_new(const char* rhs){ printf(" = \"%s\"\n", rhs);                    }
-template <typename T> void sstd_print_new::for_printn_new(const             T & rhs){ printf(" = %s\n", sstd_print_new::val2str(rhs).c_str()); }
-template <typename T> void sstd_print_new::for_printn_new(const std::vector<T>& rhs){
-    printf(" = [");
-    if(rhs.size()>=1){
-        for(uint i=0; i<rhs.size()-1; ++i){ printf("%s ", sstd_print_new::val2str(rhs[i]).c_str()); }
-        printf("%s", sstd_print_new::val2str(rhs[rhs.size()-1]).c_str());
-    }
-    printf("]\n");
-}
-//---
-template <typename T>
-void sstd_vvprint_internal(const std::vector<T>& rhs){
-    if(rhs.size()<=0){ return; }
-    
-    for(uint i=0; i<rhs.size()-1; ++i){ printf("%s ", sstd_print_new::val2str(rhs[i]).c_str()); }
-    printf("%s", sstd_print_new::val2str(rhs[rhs.size()-1]).c_str());
-}
-template <typename T>
-void sstd_print_new::print(const std::vector<std::vector<T>>& rhs){
-    if(rhs.size()==0){ printf("[[]]\n"); return; }
-    
-    printf("[");
-    for(uint64 i=0; i<rhs.size()-1; ++i){
-        printf("[");
-        sstd_vvprint_internal(rhs[i]);
-        printf("] ");
-    }
-    {
-        printf("[");
-        sstd_vvprint_internal(rhs[rhs.size()-1]);
-        printf("]");
-    }
-    printf("]\n");
-}
-template <typename T>
-void sstd_print_new::for_printn_new(const std::vector<std::vector<T>>& rhs){
-    if(rhs.size()==0){ printf(" = [[]]\n"); return; }
-    
-    printf(" = [");
-    for(uint64 i=0; i<rhs.size()-1; ++i){
-        printf("[");
-        sstd_vvprint_internal(rhs[i]);
-        printf("] ");
-    }
-    {
-        printf("[");
-        sstd_vvprint_internal(rhs[rhs.size()-1]);
-        printf("]");
-    }
-    printf("]\n");
-}
-*/
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-
-// Because of complex data type speculation of template is not possible in the other .cpp file,
-// function instances are listed below.
-/*
-void sstd::print(const                               bool     rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               char     rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               int8     rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               int16    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               int32    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               int64    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                              uint8     rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                              uint16    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                              uint32    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                              uint64    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                               float    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                              double    rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                                char  * rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const                         std::string  & rhs){ sstd_print_new::print(rhs); }
-
-void sstd::print(const std::vector<                  bool  >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  char  >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  int8  >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  int16 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  int32 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  int64 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                 uint8  >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                 uint16 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                 uint32 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                 uint64 >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                  float >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<                 double >& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<            std::string >& rhs){ sstd_print_new::print(rhs); }
-
-void sstd::print(const std::vector<std::vector<      bool >>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      char >>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      int8 >>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      int16>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      int32>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      int64>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<     uint8 >>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<     uint16>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<     uint32>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<     uint64>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<      float>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<     double>>& rhs){ sstd_print_new::print(rhs); }
-void sstd::print(const std::vector<std::vector<std::string>>& rhs){ sstd_print_new::print(rhs); }
-
-//---
-
-void sstd::for_printn_new(const                               bool     rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               char     rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               int8     rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               int16    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               int32    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               int64    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                              uint8     rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                              uint16    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                              uint32    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                              uint64    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                               float    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                              double    rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                                char  * rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const                         std::string  & rhs){ sstd_print_new::for_printn_new(rhs); }
-
-void sstd::for_printn_new(const std::vector<                  bool  >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  char  >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  int8  >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  int16 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  int32 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  int64 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                 uint8  >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                 uint16 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                 uint32 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                 uint64 >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                  float >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<                 double >& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<            std::string >& rhs){ sstd_print_new::for_printn_new(rhs); }
-
-void sstd::for_printn_new(const std::vector<std::vector<      bool >>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      char >>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      int8 >>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      int16>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      int32>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      int64>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<     uint8 >>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<     uint16>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<     uint32>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<     uint64>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<      float>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<     double>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-void sstd::for_printn_new(const std::vector<std::vector<std::string>>& rhs){ sstd_print_new::for_printn_new(rhs); }
-*/
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-
