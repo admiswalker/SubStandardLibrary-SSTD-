4ae45b7f98a43e9184ad30d01af1957b70944a33
admiswalker <admiswalker@gmail.com>
2021-12-12 14:50:50 +0900

    mv: test/measureTime.hpp to test/time/measureTime.hpp

--- main.cpp
+++ main.cpp
@@ -22,7 +22,8 @@
 #include "test/string/strmatch.hpp"
 #include "test/string/utf8.hpp"
 
-#include "test/time/time.hpp"
+#include "test/time/time.hpp"*/
+#include "test/time/measureTime.hpp"/*
 
 #include "test/vector/slice.hpp"
 #include "test/vector/stdVector_expansion.hpp" // stdVector_expansion of operators
@@ -30,8 +31,7 @@
 
 #include "test/c2py.hpp"
 #include "test/math.hpp"
-#include "test/measureTime.hpp"*/
-#include "test/pdbg.hpp"/*
+#include "test/pdbg.hpp"
 #include "test/print_printn_printn_all.hpp" // void pdbg(){ printf("======p\n"); } // #define DEBUG を定義しない場合でも，マクロでこの名前は使えなくなるので，名前空間を汚しており，本当はよくない．
 #include "test/status.hpp"
 #include "test/stdlib.hpp"
