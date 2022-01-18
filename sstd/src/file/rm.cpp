b214c5669225ee600dafb1edfcf9689768327a4b
admiswalker <admiswalker@gmail.com>
2022-01-18 23:48:40 +0900

    refactoring sstd::rm()

--- sstd/src/file/rm.cpp
+++ sstd/src/file/rm.cpp
@@ -1,20 +1,15 @@
 ﻿#include "rm.hpp"
 
+#include "glob.hpp"
 #include "path.hpp"
 #include "../pdbg.hpp"
 #include "../typeDef.h"
 
 #include <vector>
-#ifdef _WIN32
-    #include <Shlwapi.h>                // PathFileExists
-    #pragma comment(lib, "shlwapi.lib") // PathFileExists
-#else
-
-    #include <dirent.h>
-    #include <sys/stat.h> // stat(), S_ISDIR()
-    #include <string.h>   // strcmp()
-    #include <unistd.h>   // rmdir(), unlink()
-#endif
+#include <dirent.h>
+#include <sys/stat.h> // stat(), S_ISDIR()
+#include <string.h>   // strcmp()
+#include <unistd.h>   // rmdir(), unlink()
 
 //-------------------------------------------------------------------------------------------------------
 
@@ -38,208 +33,34 @@ bool sstd::rmdir(const char* pPath){
 }
 bool sstd::rmdir(const std::string& path){ return sstd::rmdir(path.c_str()); }
 
-
-//--------------------------------------------------------------------------------------------------------
-
-// -> ヘッダへ移動．
-//
-//struct pathAndType_tmp{
-//    std::string path;
-//    char type;        // 'f': file, 'd': directory
-//};
-
-//--------------------------------------------------------------------------------------------------------
-
-#ifdef _WIN32
-
-// getFilePathInDir.cpp を参考に実装する
-std::vector<std::string> fileInASingleDir(std::vector<struct pathAndType_tmp>& ret, const char* pPath){
-    std::string path_WC = pPath + std::string(R"(/*)"); // path with wild card
-
-    WIN32_FIND_DATA fd;
-    HANDLE hFind = FindFirstFileEx(path_WC.c_str(), FindExInfoStandard, &fd, FindExSearchNameMatch, NULL, 0 );
-    if(hFind==INVALID_HANDLE_VALUE){ return std::vector<std::string>(); } // No files or directories.
-
-    std::vector<std::string> dirList;
-    do{
-        if(fd.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY){ // WIN32_FIND_DATA構造体のdwFileAttributesとFILE_ATTRIBUTE_DIRECTORYのビットの論理積を取り、ファイルかディレクトリかを判断している
-            // directory found
-
-            if ((strcmp( fd.cFileName, "." )!=0) && (strcmp( fd.cFileName, ".." )!=0)){
-                // subdirectory (サブディレクトリ)
-//                printf( "subdirectory: %s\n", fd.cFileName );
-
-                std::string pathBuf = pPath+std::string(R"(/)")+fd.cFileName;
-
-                struct pathAndType_tmp retBuf;
-                retBuf.path = pathBuf;
-                retBuf.type = 'd';
-                ret.push_back(retBuf);
-
-                dirList.push_back(pathBuf);
-            }else{
-                //  . current directory (カレントディレクトリ)
-                // .. parent directory (親ディレクトリ)
-//                printf( "current or parent directory: %s\n", fd.cFileName );
-            }
-        }else{
-            // file found
-//            printf("file: %s\n", fd.cFileName);
-
-            struct pathAndType_tmp retBuf;
-            retBuf.path = pPath+std::string(R"(/)")+fd.cFileName;
-            retBuf.type = 'f';
-            ret.push_back(retBuf);
-        }
-    }while( FindNextFile(hFind, &fd) );
-
-    FindClose(hFind);
-    return dirList;
-}
-#else
-
-//----------------------------------------------------
-
-std::vector<std::string> fileInASingleDir(bool& result, std::vector<struct sstd::pathAndType_tmp>& ret, const char* pPath){
-
-    // ディレクトリを開く
-    DIR *pDir = opendir(pPath);
-    if(NULL==pDir){ sstd::pdbg("ERROR: opendir() was failed.\n"); result=false; return std::vector<std::string>(); } // there is no file or directory.
-
-    std::vector<std::string> dirList;
-    for(struct dirent* pEnt=readdir(pDir); pEnt!=0; pEnt=readdir(pDir)){
-
-        // ファイルの情報を取得
-//        printf("pEnt->d_name: %s\n", pEnt->d_name);
-//        std::string pathName = pPath;// + std::string(pEnt->d_name);
-        std::string pathName = pPath+std::string(R"(/)")+std::string(pEnt->d_name);
-//        printf("pathName: %s\n", pathName.c_str());
-        struct stat st;
-        if(stat(pathName.c_str(), &st)){ sstd::pdbg("ERROR: Failed to get stat \"%s\"\n", pathName.c_str()); result=false; break; }
-
-        if(S_ISDIR(st.st_mode)){
-            // directory found
-
-            if((strcmp(pEnt->d_name, ".")!=0) && (strcmp(pEnt->d_name, "..")!=0)){
-                // subdirectory (サブディレクトリ)
-//                printf("subdirectory: %s\n", pEnt->d_name);
-
-                std::string pathBuf = pPath+std::string(R"(/)")+pEnt->d_name;
-
-                struct sstd::pathAndType_tmp retBuf;
-                retBuf.path = pathBuf;
-                retBuf.type = 'd';
-                ret.push_back(retBuf);
-                
-                dirList.push_back(pathBuf);
-                
-                //printf("retBuf.path.c_str(): %s\n", retBuf.path.c_str());
-            }else{
-                //  . current directory (カレントディレクトリ)
-                // .. parent directory (親ディレクトリ)
-//                   printf("current or parent directory: %s\n", pEnt->d_name);
-            }
-        }else{
-            // file found
-//            printf("file: %s\n", pEnt->d_name);
-
-            struct sstd::pathAndType_tmp retBuf;
-            retBuf.path = pPath+std::string(R"(/)")+pEnt->d_name; 
-            retBuf.type = 'f';
-            ret.push_back(retBuf);
-
-            //printf("retBuf.path.c_str(): %s\n", retBuf.path.c_str());
-        }
-    }
-    
-    closedir(pDir);
-    return dirList;
-}
-#endif
-
-//--------------------------------------------------------------------------------------------------------
-
-bool sstd::getAllPath(std::vector<struct sstd::pathAndType_tmp>& ret, const char* pPath){
-    if(!sstd::pathExist(pPath)){ return false; } // there is no file or directory.
-    
-    struct pathAndType_tmp retBuf;
-    retBuf.path = pPath;
-    if(!sstd::isDir(pPath)){
-        retBuf.type='f';
-        ret.push_back(retBuf);
-        return true;
-    }
-    retBuf.type = 'd';
-    ret.push_back(retBuf);
-    
-    bool result=true;
-    std::vector<std::string> dirList;
-    dirList.push_back(pPath);
-    for(uint i=0; i<dirList.size(); ++i){
-        std::vector<std::string> buf = fileInASingleDir(result, ret, dirList[i].c_str());
-        dirList.insert(dirList.end(), buf.begin(), buf.end());
-    }
-    return result;
-}
-bool sstd::getAllPath(std::vector<std::string>& ret, const char* pPath){
-    ret.clear();
-    uint rSize=0;
-    
-    std::vector<struct sstd::pathAndType_tmp> allPath;
-    if(!sstd::getAllPath(allPath, pPath)){ sstd::pdbg("ERROR: getAllInDir() is failed\n"); return false; }
-    
-    // In order to avoid directory traversal
-    for(uint i=0; i<allPath.size(); ++i){
-        ret.push_back(std::string());
-        ret[rSize].swap(allPath[i].path); ++rSize;
-    }
-    return true;
-}
-bool sstd::getAllFile(std::vector<std::string>& ret, const char* pPath){
-    ret.clear();
-    uint rSize=0;
-    
-    std::vector<struct sstd::pathAndType_tmp> allPath;
-    if(!sstd::getAllPath(allPath, pPath)){ sstd::pdbg("ERROR: getAllInDir() is failed\n"); return false; }
-    
-    // In order to avoid directory traversal
-    for(uint i=0; i<allPath.size(); ++i){
-        if(allPath[i].type!='f'){ continue; } // remove directory
-        ret.push_back(std::string());
-        ret[rSize].swap(allPath[i].path); ++rSize;
-    }
-    return true;
-}
-bool sstd::getAllDir(std::vector<std::string>& ret, const char* pPath){
-    ret.clear();
-    uint rSize=0;
-    
-    std::vector<struct sstd::pathAndType_tmp> allPath;
-    if(!sstd::getAllPath(allPath, pPath)){ sstd::pdbg("ERROR: getAllInDir() is failed\n"); return false; }
-    
-    // In order to avoid directory traversal
-    for(uint i=0; i<allPath.size(); ++i){
-        if(allPath[i].type!='d'){ continue; } // remove directory
-        ret.push_back(std::string());
-        ret[rSize].swap(allPath[i].path); ++rSize;
-    }
-    return true;
-}
-
 //--------------------------------------------------------------------------------------------------------
 
 bool sstd::rm(const char* pPath){
-    std::vector<struct sstd::pathAndType_tmp> fileList;
-    if(!sstd::getAllPath(fileList, pPath)){ return false; } // there is no file or directory.
-
-    bool retVal = true;
-    for(int i=fileList.size()-1; i>=0; --i){
-        if      ('f'==fileList[i].type){ if(!sstd::unlink(fileList[i].path)){ retVal=false; } //ファイルの消去
-        }else if('d'==fileList[i].type){ if(!sstd::rmdir (fileList[i].path)){ retVal=false; } //ディレクトリの消去 // remove なら，ファイルかディレクトリか自動で判別して，呼び出す関数をスイッチしてくれるらしい
-        }             else             { sstd::pdbg("ERROR: sstd::rm\n"); retVal=false;         }
+    bool ret = true;
+    
+    std::vector<struct sstd::pathAndType> v_pt = sstd::glob_pt(pPath+std::string("/*"), "dfr");
+    for(uint i=0; i<v_pt.size(); ++i){
+        printf("path: %s\n", v_pt[i].path.c_str());
+        printf("type: %c\n\n", v_pt[i].type);
     }
-
-    return retVal;
+    
+    // remove under pPath
+    for(int i=v_pt.size()-1; i>=0; --i){
+        switch(v_pt[i].type){
+        case 'f': { if(!sstd::unlink(v_pt[i].path.c_str())){ret=false;} break; }
+        case 'd': { if(!sstd::rmdir (v_pt[i].path.c_str())){ret=false;} break; }
+        default: { sstd::pdbg("ERROR: sstd::rm(): sstd::glob_pt() returned unexpected file type.\n"); ret=false; break; }
+        }
+    }
+    
+    // remove myself (pPath)
+    if(sstd::isFile(pPath)){
+        if(!sstd::unlink(pPath)){ret=false;}
+    }else{
+        if(!sstd::rmdir(pPath)){ret=false;}
+    }
+    
+    return ret;
 }
 bool sstd::rm(const std::string& path){ return sstd::rm(path.c_str()); }
 
---
+++ test/file/rm.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// Do not run this test, in order not to break your system when a sstd::rm() has a bag and removing unexpected file or direcories.
+// And run this test on a external disk or virtual machine, preparing for the system corruption.
+
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+// rm
+
+TEST(rm, rm){
+//    sstd::rm(".");
+//    sstd::rm("/media/admis/KIOXIA/test_rm");
+//    sstd::rm("/media/admis/KIOXIA/test_rm");
+//    sstd::rm("/media/admis/KIOXIA/test_rm/a/al");
+}
+
