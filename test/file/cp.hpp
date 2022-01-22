9b69381e2ba8dc9c49190360d93cfd960ae8d796
admiswalker <admiswalker@gmail.com>
2022-01-23 02:37:02 +0900

    wip

--- test/file/cp.hpp
+++ test/file/cp.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 
-TEST(cp, copy_pChar_pChar){
+TEST(cp, copy_pChar_pChar_case01){
     sstd::mkdir("./tmpDir_cp");
     sstd::system("dd if=/dev/urandom of=./tmpDir_cp/test_rand.bin bs=1M count=10 > /dev/null 2>&1");
     
@@ -12,6 +12,36 @@ TEST(cp, copy_pChar_pChar){
 
     ASSERT_STREQ(hash_src.c_str(), hash_dst.c_str());
     sstd::rm("./tmpDir_cp");
+}
+TEST(cp, copy_pChar_pChar_case02_changeFileSize){
+    sstd::mkdir("./tmpDir_cp");
+    sstd::system("dd if=/dev/urandom of=./tmpDir_cp/test_rand.bin bs=1M count=10 > /dev/null 2>&1");
+    
+    sstd::copy("./tmpDir_cp/test_rand.bin", "./tmpDir_cp/test_rand_copy.bin");
+    {   
+        std::string hash_src = sstd::system_stdout("sha256sum ./tmpDir_cp/test_rand.bin | cut -d \" \" -f 1"); hash_src.pop_back();
+        std::string hash_dst = sstd::system_stdout("sha256sum ./tmpDir_cp/test_rand_copy.bin | cut -d \" \" -f 1"); hash_dst.pop_back();
+        ASSERT_STREQ(hash_src.c_str(), hash_dst.c_str());
+    }
+    
+    // change file size
+    sstd::rm("./tmpDir_cp/test_rand.bin");
+    sstd::system("dd if=/dev/urandom of=./tmpDir_cp/test_rand.bin bs=1M count=9 > /dev/null 2>&1");
+    sstd::copy("./tmpDir_cp/test_rand.bin", "./tmpDir_cp/test_rand_copy.bin");
+    {
+        std::string hash_src = sstd::system_stdout("sha256sum ./tmpDir_cp/test_rand.bin | cut -d \" \" -f 1"); hash_src.pop_back();
+        std::string hash_dst = sstd::system_stdout("sha256sum ./tmpDir_cp/test_rand_copy.bin | cut -d \" \" -f 1"); hash_dst.pop_back();
+        
+        ASSERT_STREQ(hash_src.c_str(), hash_dst.c_str());
+    }
+    sstd::rm("./tmpDir_cp");
+}
+TEST(cp, copy_pChar_pChar_case03_x){
+    sstd::mkdir("./tmpDir_cp_2");
+    sstd::system("touch ./tmpDir_cp/a.sh");
+    sstd::system("chmod +x ./tmpDir_cp/a.sh");
+    
+    sstd::copy("./tmpDir_cp/a.sh", "./tmpDir_cp/a_copy.sh");
 }
 TEST(cp, copy_str_pChar){
 }
