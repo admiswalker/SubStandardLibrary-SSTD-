554e7e95a85db819a0a8e5fd306ecda3c4af31b8
admiswalker <admiswalker@gmail.com>
2022-07-17 17:44:34 +0900

    fix type

--- sstd/src/file/file_operations.hpp
+++ sstd/src/file/file_operations.hpp
@@ -1,7 +1,8 @@
 #pragma once
 
+#include <stddef.h>
 #include "../typeDef.h"
 
 namespace sstd{
-    bool file_size(uint64& ret_size, const char* path);
+    bool file_size(size_t& ret_size, const char* path);
 }
