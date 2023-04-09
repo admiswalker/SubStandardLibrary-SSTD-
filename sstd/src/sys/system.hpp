04f00744f908b4a32dcb969c67a00bb477fa0653
admiswalker <admiswalker@gmail.com>
2023-04-09 18:34:54 +0900

    add old implimentations

--- sstd/src/sys/system.hpp
+++ sstd/src/sys/system.hpp
@@ -1,5 +1,7 @@
 #pragma once
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
 namespace sstd{
     int system(const        char* cmd);
     int system(const std::string& cmd);
@@ -13,3 +15,19 @@ namespace sstd{
     int system_stdout_stderr(std::string& ret, const        char* cmd);
     int system_stdout_stderr(std::string& ret, const std::string& cmd);
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+// Below functions are the not recommended to use. (Below functions will be delete on sstd ver 3.x.x).
+
+namespace sstd{
+    std::string system_stdout(const        char* cmd);
+    std::string system_stdout(const std::string& cmd);
+    
+    std::string system_stderr(const        char* cmd);
+    std::string system_stderr(const std::string& cmd);
+    
+    std::string system_stdout_stderr(const        char* cmd);
+    std::string system_stdout_stderr(const std::string& cmd);
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
--- test/src_test/sys/system.cpp
+++ test/src_test/sys/system.cpp
@@ -137,5 +137,53 @@ TEST(system, system_stdout_stderr_string){
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
+// Below functions are the not recommended to use. (Below functions will be delete on sstd ver 3.x.x).
+
+TEST(system_old_imp_delete_on_ver3xx, system_stdout_char){
+    std::string test01 = sstd::system_stdout(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)");
+    ASSERT_STREQ(test01.c_str(), "hello-stdout\n");
+    
+    std::string test02 = sstd::system_stdout(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)");
+    ASSERT_STREQ(test02.c_str(), "");
+}
+TEST(system_old_imp_delete_on_ver3xx, system_stdout_string){
+    std::string test01 = sstd::system_stdout(std::string(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)"));
+    ASSERT_STREQ(test01.c_str(), "hello-stdout\n");
+    
+    std::string test02 = sstd::system_stdout(std::string(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)"));
+    ASSERT_STREQ(test02.c_str(), "");
+}
+
+TEST(system_old_imp_delete_on_ver3xx, system_stderr_char){
+    std::string test01 = sstd::system_stderr(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)");
+    ASSERT_STREQ(test01.c_str(), "");
+    
+    std::string test02 = sstd::system_stderr(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)");
+    ASSERT_STREQ(test02.c_str(), "hello-stderr\n");
+}
+TEST(system_old_imp_delete_on_ver3xx, system_stderr_string){
+    std::string test01 = sstd::system_stderr(std::string(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)"));
+    ASSERT_STREQ(test01.c_str(), "");
+    
+    std::string test02 = sstd::system_stderr(std::string(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)"));
+    ASSERT_STREQ(test02.c_str(), "hello-stderr\n");
+}
+
+TEST(system_old_imp_delete_on_ver3xx, system_stdout_stderr_char){
+    std::string test01 = sstd::system_stdout_stderr(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)");
+    ASSERT_STREQ(test01.c_str(), "hello-stdout\n");
+    
+    std::string test02 = sstd::system_stdout_stderr(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)");
+    ASSERT_STREQ(test02.c_str(), "hello-stderr\n");
+}
+TEST(system_old_imp_delete_on_ver3xx, system_stdout_stderr_string){
+    std::string test01 = sstd::system_stdout_stderr(std::string(R"(sh ./test/src_test/sys/stdlib_system_stdout.sh)"));
+    ASSERT_STREQ(test01.c_str(), "hello-stdout\n");
+    
+    std::string test02 = sstd::system_stdout_stderr(std::string(R"(sh ./test/src_test/sys/stdlib_system_stderr.sh)"));
+    ASSERT_STREQ(test02.c_str(), "hello-stderr\n");
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
 
 EXECUTE_TESTS();
