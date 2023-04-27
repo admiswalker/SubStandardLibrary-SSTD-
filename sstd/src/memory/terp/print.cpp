c45a670a4c29e8aac372030432fb252f422bb157
admiswalker <admiswalker@gmail.com>
2023-04-27 23:39:23 +0900

    add TEST(memory_terp_print, list_depth1), TEST(memory_terp_print, list_depth2)

--- sstd/src/memory/terp/print.cpp
+++ sstd/src/memory/terp/print.cpp
@@ -5,20 +5,40 @@
 
 #include "../../print/print.hpp" // for debug
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+// define internal functjions
+
+void _print_terp_str(const sstd::terp::var& rhs);
+void _print_terp_list_internal(const sstd::terp::var& rhs);
+void _print_terp_list(const sstd::terp::var& rhs);
+void _print_terp_hash(const sstd::terp::var& rhs);
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
 
 void _print_terp_str(const sstd::terp::var& rhs){
     printf("%s", rhs.to<std::string>().c_str());
 }
-void _print_terp_list(const sstd::terp::var& rhs){
-    //for(auto itr=rhs.begin(); itr!=rhs.end(); ++itr){
-    //    printf("%s", itr.first_to<std::string>().c_str());
-    //}
-    for(uint i=0; i<rhs.size(); ++i){
-        printf("%s", rhs[i].to<std::string>().c_str());
+void _print_terp_list_internal(const sstd::terp::var& rhs){
+    switch(rhs.typeNum()){
+    case sstd::num_str              : { _print_terp_str (rhs); } break;
+    case sstd::num_vec_void_ptr     : { _print_terp_list(rhs); } break;
+    case sstd::num_hash_str_void_ptr: { _print_terp_hash(rhs); } break;
+    case sstd::num_null: {} break;
+    default: { sstd::pdbg("ERROR"); } break;
     }
+}
+void _print_terp_list(const sstd::terp::var& rhs){
+    printf("[");
+    if(rhs.size()>=1){
+        for(uint i=0; i<rhs.size()-1; ++i){ _print_terp_list_internal(rhs[i]); printf(" "); }
+        _print_terp_list_internal( rhs[rhs.size()-1] );
+    }
+    printf("]");
 }
 void _print_terp_hash(const sstd::terp::var& rhs){
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
 
 void sstd::print(const sstd::terp::var& rhs){
     switch(rhs.typeNum()){
@@ -31,3 +51,4 @@ void sstd::print(const sstd::terp::var& rhs){
     printf("\n");
 }
 
+//-----------------------------------------------------------------------------------------------------------------------------------------------
--- test/src_test/memory/terp/terp_print.cpp
+++ test/src_test/memory/terp/terp_print.cpp
@@ -31,14 +31,33 @@ TEST(memory_terp_print, var_num_str){
 //*
 
 // print
-TEST(memory_terp_print, list){
+TEST(memory_terp_print, list_depth1){
     sstd::terp::var a;
     a = sstd::terp::list(3);
     a[0] = "v0";
     a[1] = "v1";
     a[2] = "v2";
 
-    sstd::print(a);
+    testing::internal::CaptureStdout();
+    sstd::print(a); // TEST THIS LINE
+    std::string ret = testing::internal::GetCapturedStdout().c_str();
+    ASSERT_STREQ(ret.c_str(), "[v0 v1 v2]\n");
+}
+TEST(memory_terp_print, list_depth2){
+    sstd::terp::var a;
+    a = sstd::terp::list(4);
+    a[0] = "v0";
+    a[1] = "v1";
+    a[2] = "v2";
+    a[3] = sstd::terp::list(3);
+    a[3][0] = "v30";
+    a[3][1] = "v31";
+    a[3][2] = "v32";
+
+    testing::internal::CaptureStdout();
+    sstd::print(a); // TEST THIS LINE
+    std::string ret = testing::internal::GetCapturedStdout().c_str();
+    ASSERT_STREQ(ret.c_str(), "[v0 v1 v2 [v30 v31 v32]]\n");
 }
 
 //*/
