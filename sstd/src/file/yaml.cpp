f84b8f97f8944e84a4f9eabe234bafe4319e6d26
admiswalker <admiswalker@gmail.com>
2023-04-30 22:38:37 +0900

    wip

--- sstd/src/file/yaml.cpp
+++ sstd/src/file/yaml.cpp
@@ -10,10 +10,24 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
 #define NUM_NULL 255
-#define NUM_VEC 1
+#define NUM_LIST 1
 #define NUM_HASH 2
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
+
+bool _isEmpty(const std::string& s){
+    for(uint i=0; i<s.size(); ++i){
+        if(s[i]!=' '){ return false; }
+    }
+    return true;
+}
+void _skip_empty_line(uint& idx, const std::vector<std::string>& ls){
+    for(; idx<ls.size(); ++idx){
+        if(!_isEmpty(ls[idx])){break;} // skip empty lines
+    }
+}
+
+//---
 
 uint _count_head_space(std::string& s){
     uint c=0;
@@ -23,61 +37,65 @@ uint _count_head_space(std::string& s){
     }
     return c;
 }
-uint _data_type(uint hsc, std::string& s){
+uint _data_type(uint hsc, const std::string& s){
     if(s.size() <= hsc+2){ return NUM_NULL; }
-    if(s[hsc]=='-' && s[hsc+1]==' '){ return NUM_VEC; }
+    if(s[hsc]=='-' && s[hsc+1]==' '){ return NUM_LIST; }
 
     if(sstd::charIn(':', s)){ return NUM_HASH; }
 
     return NUM_NULL;
 }
 std::string _get_str_from_vec(const std::string& s){
+    if(s.size()<=2){ return std::string(); }
     return &s[2];
 }
 void _get_str_from_hash(std::string& key, std::string& val, const std::string& s){
 }
 
-void _set_val(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint ls_begin){
-    for(uint i=ls_begin; i<ls.size(); ++i){
+void _set_val_list(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint idx){
+    for(uint i=idx; i<ls.size(); ++i){
         std::string s = _get_str_from_vec(ls[i]);
+        if(_isEmpty(ls[i])){ continue; }
+        
         sstd::printn(s);
         ret.push_back(s.c_str());
+    }
+}
+void _set_val_hash(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint idx){
+}
+void _set_val(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint idx){
+    if(idx>=ls.size()){ return; }
+    uint typeNum = _data_type(hsc, ls[idx]);
+    
+    switch(typeNum){
+    case NUM_LIST: { ret=sstd::terp::list(); _set_val_list(ret, ls, hsc, idx); } break;
+    case NUM_HASH: { ret=sstd::terp::hash(); _set_val_hash(ret, ls, hsc, idx); } break;
+    default: { sstd::pdbg("ERROR"); } break;
     }
 }
 
 sstd::terp::var sstd::yaml_from_str(const        char* s){
     sstd::terp::var ret;
-    //std::vector<std::string> ls = sstd::splitByLine(s);                 // ls: line string
-    //std::vector<uint> ln(ls.size()); std::iota(ln.begin(),ln.end(), 1); // ln: line number
-    //sstd::remove(ls, ln);
-    //sstd::printn(ls);
-    //sstd::printn(ln);
 
     std::vector<std::string> ls = sstd::splitByLine(s); // line string
-    //std::vector<uint> hsc(ls.size()); // head space count
-    // ls = _rm_comment(ls);
+    //sstd::printn(ls);
 
-    if(ls.size()==0){ return ret; }
-    uint typeNum = _data_type(0, ls[0]);
+    uint idx=0;
+    _skip_empty_line(idx, ls);
+    /*
+    if(idx>=ls.size()){ return ret; }
+    uint typeNum = _data_type(0, ls[idx]);
     
     switch(typeNum){
-    case NUM_VEC: { ret=sstd::terp::list(); } break;
+    case NUM_LIST: { ret=sstd::terp::list(); } break;
+    case NUM_HASH: { ret=sstd::terp::hash(); } break;
     default: { sstd::pdbg("ERROR"); } break;
     }
 
-    _set_val(ret, ls, 0, (uint)0);
-    /*
-    for(uint i=0; i<ls.size(); ++i){
-        //sstd::printn(ls[i]);
-        
-        uint hsc = _count_head_space(ls[i]);
-        //sstd::printn(hsc);
-        //if(hsc!=0){ continue; }
-
-        uint tn = _data_type(hsc, ls[i]);
-        //sstd::printn(tn);
-    }
+    _set_val(ret, ls, 0, (uint)idx);
     */
+    _set_val(ret, ls, 0, (uint)idx);
+    
     return ret;
 }
 sstd::terp::var sstd::yaml_from_str(const std::string& s){ return sstd::yaml_from_str(s.c_str()); }
--- test/src_test/file/yaml.cpp
+++ test/src_test/file/yaml.cpp
@@ -3,10 +3,22 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
-TEST(yaml, test01){
-    std::string s=R"(- a
+TEST(yaml, push_back_to_list){
+    std::string s=R"(
+- a
 - b
+
 - c
+)";
+    sstd::terp::var yml = sstd::yaml_from_str(s); // TEST THIS LINE
+    sstd::printn(yml);
+}
+TEST(yaml, push_back_to_hash){
+    std::string s=R"(
+k1: v1
+k2: v2
+
+k3: v3
 )";
     sstd::terp::var yml = sstd::yaml_from_str(s); // TEST THIS LINE
     sstd::printn(yml);
