3c43fbfd6c7ba6af885483ffa31bbdcadc52b097
admiswalker <admiswalker@gmail.com>
2023-04-30 19:10:33 +0900

    rm unused lines

--- sstd/src/file/yaml.hpp
+++ sstd/src/file/yaml.hpp
@@ -6,27 +6,11 @@
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
 namespace sstd{
-//    class yaml;
+//    sstd::terp::var yaml(const        char* p_file_path);
+//    sstd::terp::var yaml(const std::stirng&   file_path);
+    
     sstd::terp::var yaml_from_str(const        char* s);
     sstd::terp::var yaml_from_str(const std::string& s);
-
-//    yaml str2yaml(std::string& s);
 }
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------
-/*
-class sstd::yaml{
-private:
-    sstd::terp::var obj;
-public:
-    yaml();
-//    yaml(const        char* p_file_path);
-//    yaml(const std::stirng&   file_path);
-    ~yaml();
-    
-    void init_from_str(const        char* s);
-    void init_from_str(const std::string& s);
-//    void str2yaml();
-};
-*/
 //-----------------------------------------------------------------------------------------------------------------------------------------------
