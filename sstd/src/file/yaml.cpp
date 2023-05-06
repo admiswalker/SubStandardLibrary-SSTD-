71963e9b464d2860d8217851af40783a8e3ff458
admiswalker <admiswalker@gmail.com>
2023-05-07 04:43:47 +0900

    rm unused lines

--- sstd/src/file/yaml.cpp
+++ sstd/src/file/yaml.cpp
@@ -28,11 +28,6 @@ bool _is_empty(const std::string& s){
     }
     return true;
 }
-void _skip_empty_line(uint& idx, const std::vector<std::string>& ls){
-    for(; idx<ls.size(); ++idx){
-        if(!_is_empty(ls[idx])){break;} // skip empty lines
-    }
-}
 
 //---
 
@@ -99,135 +94,6 @@ std::string _rm_hyphen(std::string s){
 }
 
 //---
-
-std::string _get_list_str(const std::string& s, uint hsc){
-    if(s.size()<=hsc){ return std::string(); }
-    return &s[hsc];
-}
-
-//---
-
-void _set_val(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint& idx); // forward declaration
-
-void _push_back_for_list(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint& idx){
-    std::string s;
-    s = _get_list_str(ls[idx], hsc);
-    s = _rm_comment(s);
-    if(_is_empty(s)){ return; }
-
-    ret.push_back(s.c_str());
-}
-void _set_val_str(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint& idx){
-    for(uint i=idx; i<ls.size(); ++i){
-        std::string s;
-        s = _get_list_str(ls[i], hsc);
-        s = _rm_comment(s);
-        if(_is_empty(s)){ continue; }
-
-        if(ret.typeNum()!=sstd::num_null){ sstd::pdbg_err("ERROR\n"); break; }
-        ret = s.c_str();
-    }
-}
-void _set_val(sstd::terp::var& ret, const std::string& key_prev, std::string s, uint typeNum){
-    if(typeNum==NUM_HASH){
-        std::vector<std::string> v = sstd::split(s, ':');
-        for(uint i=0; i<v.size(); ++i){ v[i]=sstd::strip(v[i]); } // remove spaces
-        if(v.size()!=2){ sstd::pdbg_err("ERROR\n"); return; }
-
-        ret[sstd::rstrip(_rm_hyphen(v[0])).c_str()] = sstd::strip(v[1]).c_str();
-    }else if(typeNum==NUM_LIST){
-        s = _rm_hyphen(s);
-        ret.push_back(s.c_str());
-    }else{
-        sstd::printn("ERROR\n");
-    }
-}
-void _set_val_list(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc_base, uint& idx){
-    std::string key_prev;
-    for(; idx<ls.size(); ++idx){
-        std::string s;
-        s = _rm_comment(ls[idx]);
-        if(_is_empty(s)){ continue; }
-        uint typeNum = _data_type(s);
-        
-        uint hsc = _head_space_count(s);
-        if(hsc>hsc_base){
-            if(typeNum==NUM_HASH){ ret.push_back(sstd::terp::hash()); }
-            
-            const sstd::terp::var& ret_tmp=ret[ret.size()-1];
-            _set_val((sstd::terp::var&)ret_tmp, ls, hsc, idx);
-            continue;
-        }else if(hsc<hsc_base){
-            --idx;
-            break;
-        }
-        
-        if(s=="-"){
-            ret.push_back(sstd::terp::list());
-            continue;
-        }
-        _set_val(ret, key_prev, s, typeNum);
-    }
-}
-void _set_val_hash(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc_base, uint& idx){
-    std::string key_prev;
-    for(; idx<ls.size(); ++idx){
-        std::string s;
-        s = _rm_comment(ls[idx]);
-        if(_is_empty(s)){ continue; }
-        uint typeNum = _data_type(s);
-        
-        uint hsc = _head_space_count(s);
-        if(hsc>hsc_base){
-            if      (typeNum==NUM_HASH){ ret[key_prev.c_str()] = sstd::terp::hash();
-            }else if(typeNum==NUM_LIST){ ret[key_prev.c_str()] = sstd::terp::list(); }
-            
-            const sstd::terp::var& ret_tmp=ret[key_prev.c_str()];
-            _set_val((sstd::terp::var&)ret_tmp, ls, hsc, idx);
-            continue;
-        }else if(hsc<hsc_base){
-            --idx;
-            break;
-        }
-        
-        if(typeNum==NUM_HASH && sstd::rstrip(s)[s.size()-1]==':'){
-            std::vector<std::string> v = sstd::split(s, ':');
-            for(uint i=0; i<v.size(); ++i){ v[i]=sstd::strip(v[i]); }
-            key_prev = v[0];
-            continue;
-        }
-        _set_val(ret, key_prev, s, typeNum);
-    }
-}
-
-//---
-
-void _set_val(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint& idx){
-    if(idx>=ls.size()){ return; }
-    uint typeNum = _data_type(ls[idx]);
-    //sstd::printn(typeNum);
-    
-    switch(typeNum){
-    case NUM_NULL: { _set_val_str (ret, ls, hsc, idx); } break;
-    case NUM_LIST: { _set_val_list(ret, ls, hsc, idx); } break;
-    case NUM_HASH: { _set_val_hash(ret, ls, hsc, idx); } break;
-    default: { sstd::pdbg_err("ERROR\n"); } break;
-    }
-}
-
-//---
-
-void _set_list_hash(sstd::terp::var& ret, const std::vector<std::string>& ls, uint hsc, uint& idx){
-    if(idx>=ls.size()){ return; }
-    uint typeNum = _data_type(ls[idx]);
-    
-    switch(typeNum){
-    case NUM_NULL: {                         } break;
-    case NUM_LIST: { ret=sstd::terp::list(); } break;
-    case NUM_HASH: { ret=sstd::terp::hash(); } break;
-    default: { sstd::pdbg_err("ERROR\n"); } break;
-    }
-}
 
 //---
 
@@ -413,7 +279,8 @@ void _construct_terpVar(sstd::terp::var& ret, const std::vector<struct command>&
             default: { sstd::pdbg_err("Unexpected data type\n"); } break;
             }
         }
-        
+
+        // check indent
         if(v_cmd[i].hsc > hsc_base){
             v_hsc.push_back(v_cmd[i].hsc);
             --i;
@@ -425,7 +292,7 @@ void _construct_terpVar(sstd::terp::var& ret, const std::vector<struct command>&
             continue;
         }
         
-        // set dst value
+        // set dst and value
         switch(v_cmd[i].verb){
         case 's': {
             if(var.typeNum()!=sstd::num_null){ sstd::pdbg_err("OverWritting the existing data. (String data type can only take one data.)\n"); break; }
@@ -464,13 +331,6 @@ sstd::terp::var sstd::yaml_from_str(const        char* s){
     std::vector<struct command> v_cmd = _parse_yaml(ls);
     sstd::terp::var ret2;
     _construct_terpVar(ret2, v_cmd);
-    
-    /*
-    uint idx=0;
-    _skip_empty_line(idx, ls);
-    _set_list_hash(ret, ls, 0, idx);
-    _set_val(ret, ls, 0, idx);
-    */
     
     return ret2;
 }
