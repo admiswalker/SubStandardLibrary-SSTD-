883a895d3765e79a2e498a390c059dd6d59ef962
admiswalker <admiswalker@gmail.com>
2023-08-26 23:49:53 +0900

    add: rmEmpty_l, rmEmpty_l_ow, rmEmpty_r, rmEmpty_r_ow

--- sstd/src/container/vector/vec_manipulation.hpp
+++ sstd/src/container/vector/vec_manipulation.hpp
@@ -4,50 +4,124 @@
 #include <vector>
 
 namespace sstd{
-    template<typename T>
-    std::vector<T> rmEmpty(const std::vector<T>& vec){
-        std::vector<T> ret_vec;
-        for(uint i=0; i<vec.size(); ++i){
-            if(vec[i].size()==0){ continue; }
-            ret_vec.push_back(vec[i]);
-        }
-        return ret_vec;
-    }
-    template<typename T>
-    void rmEmpty_ow(std::vector<T>& vec){
-        uint i_ret=0;
-        for(uint i=0; i<vec.size(); ++i){
-            if(vec[i].size()==0){ continue; }
-            std::swap(vec[i_ret], vec[i]); ++i_ret;
-        }
-        vec.resize(i_ret);
-    }
+    //-----------------------------------------------------------------------------------------------------------------------------------------------
+    
+    template<typename T> std::vector<T> rmEmpty   (const std::vector<T>& vec);
+    template<typename T> void           rmEmpty_ow(      std::vector<T>& vec);
+    
+    //---
 
+    template<typename T> std::vector<T> rmEmpty_l   (const std::vector<T>& vec);
+    template<typename T> void           rmEmpty_l_ow(      std::vector<T>& vec);
+    
+    //---
+    
+    template<typename T> std::vector<T> rmEmpty_r   (const std::vector<T>& vec);
+    template<typename T> void           rmEmpty_r_ow(      std::vector<T>& vec);
+    
     //-----------------------------------------------------------------------------------------------------------------------------------------------
     // rmEmpty_ow(arg1, arg2, ...) of multiple vector arguments
     
-    template<typename T>
-    inline void _rmEmpty_ow(const std::vector<bool>& v, std::vector<T>& head){
-        uint i_ret=0;
-        for(uint i=0; i<v.size() && i<head.size(); ++i){
-            if(v[i]==false){ continue; }
-            std::swap(head[i_ret], head[i]); ++i_ret;
-        }
-        head.resize(i_ret);
-    }
-    template<typename Head, typename... Tail>
-    inline void _rmEmpty_ow(const std::vector<bool>& v, Head&& head, Tail&&... tail){
-        sstd::_rmEmpty_ow(v, std::forward<Head>(head));
-        sstd::_rmEmpty_ow(v, std::forward<Tail>(tail)...);
-    }
-    template<typename Head, typename... Tail>
-    inline void rmEmpty_ow(Head&& head, Tail&&... tail){ // Ascending: 昇順: 0, 1, 2, ...
-        std::vector<bool> v(head.size(), true);
-        for(uint i=0; i<head.size(); ++i){ if(head[i].size()==0){v[i]=false;} }
-        
-        sstd::_rmEmpty_ow(v, std::forward<Head>(head));
-        sstd::_rmEmpty_ow(v, std::forward<Tail>(tail)...);
-    }
+    template<typename T>                      inline void _rmEmpty_ow(const std::vector<bool>& v, std::vector<T>& head);
+    template<typename Head, typename... Tail> inline void _rmEmpty_ow(const std::vector<bool>& v, Head&& head, Tail&&... tail);
+    template<typename Head, typename... Tail> inline void rmEmpty_ow(Head&& head, Tail&&... tail);
     
     //-----------------------------------------------------------------------------------------------------------------------------------------------
 }
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+
+template<typename T>
+std::vector<T> sstd::rmEmpty(const std::vector<T>& vec){
+    std::vector<T> ret_vec;
+    for(uint i=0; i<vec.size(); ++i){
+        if(vec[i].size()==0){ continue; }
+        ret_vec.push_back(vec[i]);
+    }
+    return ret_vec;
+}
+
+template<typename T>
+void sstd::rmEmpty_ow(std::vector<T>& vec){
+    uint i_ret=0;
+    for(uint i=0; i<vec.size(); ++i){
+        if(vec[i].size()==0){ continue; }
+        std::swap(vec[i_ret], vec[i]); ++i_ret;
+    }
+    vec.resize(i_ret);
+}
+
+//---
+
+template<typename T>
+std::vector<T> sstd::rmEmpty_l(const std::vector<T>& vec){
+    uint i=0;
+    for(; i<vec.size(); ++i){
+        if(vec[i].size()!=0){ break; }
+    }
+    if(i>=vec.size()){ return std::vector<T>(); }
+        
+    return std::vector<T>(vec.begin()+i, vec.end());
+}
+template<typename T>
+void sstd::rmEmpty_l_ow(std::vector<T>& vec){
+    uint i=0;
+    for(; i<vec.size(); ++i){
+        if(vec[i].size()==0){ continue; }else{ break; }
+    }
+        
+    uint i_ret=0;
+    for(; i<vec.size(); ++i){
+        std::swap(vec[i_ret], vec[i]); ++i_ret;
+    }
+    vec.resize(i_ret);
+}
+
+//---
+
+template<typename T>
+std::vector<T> sstd::rmEmpty_r(const std::vector<T>& vec){
+    int i=vec.size()-1;
+    for(; i>=0; --i){
+        if(vec[i].size()!=0){ break; }
+    }
+    if(i<0){ return std::vector<T>(); }
+        
+    return std::vector<T>(vec.begin(), vec.begin()+i+1);
+}
+template<typename T>
+void sstd::rmEmpty_r_ow(std::vector<T>& vec){
+    int i=vec.size()-1;
+    for(; i>=0; --i){
+        if(vec[i].size()!=0){ break; }
+        vec.pop_back();
+    }
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
+
+template<typename T>
+inline void sstd::_rmEmpty_ow(const std::vector<bool>& v, std::vector<T>& head){
+    uint i_ret=0;
+    for(uint i=0; i<v.size() && i<head.size(); ++i){
+        if(v[i]==false){ continue; }
+        std::swap(head[i_ret], head[i]); ++i_ret;
+    }
+    head.resize(i_ret);
+}
+template<typename Head, typename... Tail>
+inline void sstd::_rmEmpty_ow(const std::vector<bool>& v, Head&& head, Tail&&... tail){
+    sstd::_rmEmpty_ow(v, std::forward<Head>(head));
+    sstd::_rmEmpty_ow(v, std::forward<Tail>(tail)...);
+}
+template<typename Head, typename... Tail>
+inline void sstd::rmEmpty_ow(Head&& head, Tail&&... tail){
+    std::vector<bool> v(head.size(), true);
+    for(uint i=0; i<head.size(); ++i){ if(head[i].size()==0){v[i]=false;} }
+        
+    sstd::_rmEmpty_ow(v, std::forward<Head>(head));
+    sstd::_rmEmpty_ow(v, std::forward<Tail>(tail)...);
+}
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------
