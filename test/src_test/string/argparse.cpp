f344005ed53453c04c3ff6a24ab4dcb5697188c6
admiswalker <admiswalker@gmail.com>
2025-09-07 23:09:43 +0900

    add drafts

--- test/src_test/string/argparse.cpp
+++ test/src_test/string/argparse.cpp
@@ -3,70 +3,7 @@
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
 
-#define ARG_ID_NULL 0
-#define ARG_ID_GET_LINES 1
-#define ARG_ID_GET_LINE  2
-
 TEST(argparse, example01){
-    sstd::argparse ap;
-    
-    int res_dummy; std::vector<int> res_v; int res_i;
-//    ap.add_required_arg(res_dummy, ARG_ID_NULL,  "",           0, int(0)              );
-    ap.add_required_arg(res_v, ARG_ID_GET_LINES, "get lines", -1, std::vector<int>({}));
-    ap.add_required_arg(res_i, ARG_ID_GET_LINE , "get line",   1, int(0)              );
-    // Memo:     return value,           switch index, command, arg length, default value
-    
-    bool rm_hs=false, rm_ts=false;
-    ap.add_optional_arg(rm_hs, "-h", "--rm-head-spaces", 1, false);
-    ap.add_optional_arg(rm_ts, "-t", "--rm-tail-spaces", 1, false);
-    
-    int res = ap.parse(argc, argv);
-    switch(res){
-    case ARG_ID_GET_LINES: { /* process get lines */ } break;
-    case ARG_ID_GET_LINE : { /* process get line */ } break;
-    default : { printf("error: %s\n", ap.error.c_str()); }
-    }
-    
-    return 0;
-}
-
-#undef ARG_ID_GET_LINES
-#undef ARG_ID_GET_LINE
-
-//---
-
-TEST(argparse, example01_2){
-    enum{
-         ARG_ID_NULL,
-         ARG_ID_GET_LINES,
-         ARG_ID_GET_LINE
-    };
-    
-    sstd::argparse ap;
-    
-    int res_dummy; std::vector<int> res_v; int res_i;
-    ap.add_required_arg(res_dummy, ARG_ID_NULL     ,          "",  0, int(0)              );
-    ap.add_required_arg(res_v,     ARG_ID_GET_LINES, "get lines", -1, std::vector<int>({}));
-    ap.add_required_arg(res_i,     ARG_ID_GET_LINE , "get line" ,  1, int(0)              );
-    // Memo:     return value,           switch index, command, arg length, default value
-    
-    bool rm_hs=false, rm_ts=false;
-    ap.add_optional_arg(rm_hs, "-h", "--rm-head-spaces", 1, false);
-    ap.add_optional_arg(rm_ts, "-t", "--rm-tail-spaces", 1, false);
-    
-    int res = ap.parse(argc, argv);
-    switch(res){
-    case ARG_ID_GET_LINES: { /* process get lines */ } break;
-    case ARG_ID_GET_LINE : { /* process get line */ } break;
-    default : { printf("error: %s\n", ap.error.c_str()); }
-    }
-    
-    return 0;
-}
-
-//---
-
-TEST(argparse, example01_2){
     sstd::argparse ap;
     
     int res_dummy; std::vector<int> res_v; int res_i;
@@ -97,57 +34,113 @@ TEST(argparse, example01_2){
 
 TEST(argparse, example02){
     sstd::argparse ap;
-    ap.add_required_arg("get lines", -1);
-    ap.add_required_arg("get line", 1);
-    ap.add_optional_arg("-h", "--rm-head-spaces", 1);
-    ap.add_optional_arg("-t", "--rm-tail-spaces", 1);
     
-    std::vector<std::string> res_v;
-    std::unordered_map<std::string, std::vector<std::string>> res_opt;
-    bool res = ap.parse(res_v, res_opt, argc, argv);
+    std::vector<int> res_v; int res_i;
+    int SW_ID_NULL      = ap.add_required_arg("");
+    int SW_ID_GET_LINES = ap.add_required_arg("get lines", -1, res_v, std::vector<int>({}));
+    int SW_ID_GET_LINE  = ap.add_required_arg("get line" ,  1, res_i, int(0)              );
+    // Memo:     return value,           switch index, command, arg length, default value
     
-    if(sstd::strcmp(res_v[0], "get")){
-        if(sstd::strcmp(res_v[1], "lines")){
-        }else if(sstd::strcmp(res_v[1], "line")){
-        }else{
-            print("error\n");
-        }
-    }else{
-        print("error\n");
+    bool res_rm_hs, res_rm_ts;
+    ap.add_optional_arg("-h", "--rm-head-spaces", 1, res_rm_hs, false);
+    ap.add_optional_arg("-t", "--rm-tail-spaces", 1, res_rm_ts, false);
+    if(ap.error.size()!=0){
+        print(ap.error.c_str());
     }
+    
+    int sw_id = ap.parse(argc, argv);
+    switch(sw_id){
+    case SW_ID_GET_NULL : { print_help(); } break;
+    case SW_ID_GET_LINES: { /* process get lines */ } break;
+    case SW_ID_GET_LINE : { /* process get line */ } break;
+    default : { printf("error: %s\n", ap.error.c_str()); }
+    }
+    
+    return 0;
 }
 
 //---
 
 TEST(argparse, example03){
-    std::vector<std::string> res_v;
-    std::unordered_map<std::string, std::vector<std::string>> res_opt;
-    bool res = sstd::argparse(res_v, res_opt, argc, argv);
-    if(res){ printf("error\n"); return; }
-
-    if(res_v.size()<=1){ printf("error\n"); return; }
+    sstd::argparse ap;
     
-    if(res_v[0]=="get")){
-        if      (res_v[1]=="lines"){
-            
-        }else if(res_v[1], "line" ){
-            
-        }else{
-            print("error\n");
-        }
-    }else{
-        print("error\n");
+    int SW_ID_NULL      = ap.add_required_arg("");
+    int SW_ID_GET_LINES = ap.add_required_arg("get lines", -1, std::vector<int>({}));
+    int SW_ID_GET_LINE  = ap.add_required_arg("get line" ,  1, int(0)              );
+    // Memo:     return value,           switch index, command, arg length, default value
+    
+    int OPT_ID_H = ap.add_optional_arg("-h", "--rm-head-spaces", 1, false);
+    int OPT_ID_T = ap.add_optional_arg("-t", "--rm-tail-spaces", 1, false);
+    if(ap.error.size()!=0){
+        print(ap.error.c_str());
     }
+    
+    int sw_id = ap.parse(argc, argv);
+    switch(sw_id){
+    case SW_ID_GET_NULL : { print_help(); } break;
+    case SW_ID_GET_LINES: {
+        /* process get lines */
+        
+        std::vector<int>& res_v = ap[SW_ID_GET_LINES];
+        bool& res_rm_hs = ap[OPT_ID_H];
+        bool& res_rm_ts = ap[OPT_ID_T];
+        
+        
+    } break;
+    case SW_ID_GET_LINE : {
+        /* process get line */
+        
+        int& res_i = ap[SW_ID_GET_LINE];
+        
+    } break;
+    default : { printf("error: %s\n", ap.error.c_str()); }
+    }
+    
+    return 0;
 }
 
 //---
 
-TEST(argparse, example04){
-}
-
-//---
-
-TEST(argparse, example05){
+TEST(argparse, example03){
+    sstd::argparse ap;
+    
+    int SW_ID_NULL      = ap.add_required_arg("");
+    int SW_ID_GET_LINES = ap.add_required_arg("get lines", -1);
+    int SW_ID_GET_LINE  = ap.add_required_arg("get line" ,  1);
+    // Memo:     return value,           switch index, command, arg length, default value
+    
+    bool res_rm_hs, res_rm_ts;
+    ap.add_optional_arg("-h", "--rm-head-spaces", 1, res_rm_hs, false);
+    ap.add_optional_arg("-t", "--rm-tail-spaces", 1, res_rm_ts, false);
+//    int OPT_ID_H = ap.add_optional_arg("-h", "--rm-head-spaces", 1, false);
+//    int OPT_ID_T = ap.add_optional_arg("-t", "--rm-tail-spaces", 1, false);
+    if(ap.error.size()!=0){
+        print(ap.error.c_str());
+    }
+    
+    std::vector<std::string> res_vs;
+    int sw_id = ap.parse(res_vs, argc, argv);
+    
+    switch(sw_id){
+    case SW_ID_GET_NULL : { print_help(); } break;
+    case SW_ID_GET_LINES: {
+        /* process get lines */
+        
+        std::vector<int> arg_vi;
+        if(!sstd::s2i(res_vs)){ printf("error\n"); }
+        
+        
+    } break;
+    case SW_ID_GET_LINE : {
+        /* process get line */
+        
+        int arg_i = sstd::s2i(res_vs[0]);
+        
+    } break;
+    default : { printf("error: %s\n", ap.error.c_str()); }
+    }
+    
+    return 0;
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------
