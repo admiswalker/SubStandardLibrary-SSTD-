35dce037d9616faaffe4d586b497c9a65da13ed6
admiswalker <admiswalker@gmail.com>
2025-10-19 23:13:20 +0900

    rm unused lines

--- sstd/src/string/argparse.cpp
+++ sstd/src/string/argparse.cpp
@@ -69,21 +69,12 @@ int _is_separator(const std::string& s, const std::unordered_map<std::string,uin
 
 bool _parse_argc_argv(
                       // return variables:
-//                      std::string& err,
-//                      uint& cmdIdx,
                       std::vector<std::string>& res_cmdArgs,
-//                      std::vector<uint>& v_optIdx,
                       std::vector<std::vector<std::string>>& res_vOptArgs_short,
                       std::vector<std::vector<std::string>>& res_vOptArgs_full,
                       
                       // input variables:
                       const std::unordered_map<std::string,uint>& ht_cmd2idx,
-//                      const std::unordered_map<std::string,uint>& ht_opt2idx_full,
-//                      const std::unordered_map<std::string,uint>& ht_opt2idx_short,
-                      
-//                      const std::vector<struct sstd::arg_rule::cmd_rule>& arg_vCmd,
-//                      const std::vector<struct sstd::arg_rule::opt_rule>& arg_vOpt,
-                      
                       const int argc, const char* argv[])
 {
     int prev_type=0;
@@ -108,106 +99,6 @@ bool _parse_argc_argv(
     }
     
     return true;
-
-    //---
-
-    bool tf_opt_short = false;
-    bool tf_opt_full  = false;
-    bool tf_cmd       = false;
-    for(uint i=1; i<argc; ++i){
-        std::string s = argv[i];
-        if( (_is_short_opt(s)||_is_full_opt(s)) && res_args.size()!=0 ){
-            ;
-            std::vector<std::string> tmp;
-            res_args;
-        }else if( _is_cmd( s, ht_cmd2idx ) ){
-            ;
-        }
-
-        res_args.push_back( std::move( s ) );
-    }
-    _is_separator();
-    
-    //---
-    
-    std::vector<std::string> vArg;
-    for(uint i=1; i<argc; ++i){ vArg.push_back(argv[i]); } // Skip exe file name by `i=1`.
-    
-//    for(uint i=1; i<argc; ++i){
-    for(uint i=0; i<vArg.size(); ++i){
-//    std::string arg = argv[i];
-        
-        if(_is_short_opt(vArg[i])){
-//            vArg[i] = vArg[i] && sstd::slice(1, sstd::end()); // rm '-' // あとで追加実装する
-            vArg[i] = (const char*)&vArg[i][1]; // rm '-'
-            std::string opts;
-            std::swap(opts, vArg[i]); // Fill `vArg[i]` empty.
-
-            if(opts.size()==1){
-                auto itr = ht_opt2idx_short.find( "-"+opts );
-                if(itr==ht_opt2idx_short.end()){
-                    err = "ERROR: _parse_argc_argv() failed. User input argument `"+arg[1]+"` does NOT defined by `sstd::arg_rule::opt()`.";
-                    return false;
-                }
-                v_optIdx.push_back( itr->second );
-                v_optArgs.push_back( std::vector<std::string>() );
-            }
-            for(uint is=1; is<opts.size(); ++is){
-                auto itr = ht_opt2idx_short.find( "-"+opts[is] );
-                if(itr==ht_opt2idx_short.end()){
-                    err = "ERROR: _parse_argc_argv() failed. User input argument `-"+opts[is]+"` does NOT defined by `sstd::arg_rule::opt()`.";
-                    return false;
-                }
-
-                uint optIdx = itr->second;
-                v_optIdx.push_back( optIdx );
-                
-                int opt_len = arg_vOpt[ optIdx ].opt_len;
-                if(opt_len==-1){ // means that the option has variable length argument
-                    opt_len=0;
-                    
-                    
-                }
-//                vArg && sstd::slice_mv(); // move 後の値の保証がない？ -> sstd::slice_sw() が必要? -> 初期値と sw する
-                if( vArg.size() >= (i+opt_len) ){
-                    err = "ERROR: _parse_argc_argv() failed. User input option `-"+opts[is]+"`, defined by `sstd::arg_rule::opt()` requires "+std::itos(opt_len)+" arguments, but there is only "++" arguments.";
-                    return false;
-                }
-                std::vector<std::string> optArgs = vArg && sstd::slice_sw(i, i+opt_len);
-                v_optArgs.push_back( std::move(optArgs) );
-            }
-            
-            
-        }else if(_is_full_opt(arg)){
-            ;
-        }
-    }
-
-    sstd::rmEmpty_ow( vArg ); // This line extracts `cmdArgs` by removing empty items which are filled empty by option rule parsing process.
-    std::swap( cmdArgs, vArg );
-    return true;
-
-    //---
-    
-    std::vector<std::string> res_v;
-//    std::vector<std::string> vOpt_short;
-
-    for(uint i=0; i<vArg.size(); ++i){
-        std::string& arg = vArg[i];
-        if      (arg.starts_with("--")){
-            auto itr = arg_vOpt.find( arg );
-            if(itr==arg_vOpt.end()){ err="ERROR: The `"+arg+"` option is not defined."; return false; }
-            
-            ;
-            
-//        }else if(vArg[i].starts_with("-" )){
-//            ;
-        }else{
-            ;
-        }
-    }
-    
-    return true;
 }
 #undef NOT_A_SEPARATOR
 #undef CMD
@@ -223,8 +114,6 @@ int sstd::argparse::_parse(const int argc, const char* argv[]
                            , const std::vector<struct sstd::arg_rule::cmd_rule>& arg_vCmd
                            , const std::vector<struct sstd::arg_rule::opt_rule>& arg_vOpt)
 {
-//    std::vector<std::string> vArg;
-//    for(uint i=0; i<argc; ++i){ vArg.push_back(argv[i]); }
 
     // Process arg_vCmd
     std::unordered_map<std::string,uint> ht_cmd2idx;
